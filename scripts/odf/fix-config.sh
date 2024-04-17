6a5438c50d96a67997212a97974e214423cbbb40
SUDEESH JOHN <sudeeshjohn@in.ibm.com>
2024-04-17 19:52:19 +0530

    Fixing ODF upgrade script

--- scripts/odf/fix-config.sh
+++ scripts/odf/fix-config.sh
@@ -2,8 +2,7 @@
 ##
 ## This scripts updates the env_vars.sh and ocs-ci-conf.yaml after ODF upgrade
 ##
-pip3 install yq
-. env_vars.sh; export OCS_VERSION=`echo ${UPGRADE_OCS_VERSION} | cut -d "." -f 1-2`; export OCS_CSV_CHANNEL=stable-$OCS_VERSION; yq -y -i ".DEPLOYMENT.ocs_csv_channel |= env.OCS_CSV_CHANNEL" /root/ocs-ci-conf.yaml; yq -y -i ".ENV_DATA.ocs_version |= env.OCS_VERSION" /root/ocs-ci-conf.yaml ;  sed -i "s|log_dir:.*$|log_dir: /root/logs-ocs-ci/"$OCS_VERSION"|g"  /root/ocs-ci-conf.yaml
-cat /root/ocs-ci-conf.yaml
-. env_vars.sh; CURRENT_OCS_VERSION=`echo ${UPGRADE_OCS_VERSION} | cut -d "." -f 1-2`; sed -i "s|export OCS_VERSION=.*$|export OCS_VERSION=${CURRENT_OCS_VERSION}|g" /root/env_vars.sh
-cat /root/env_vars.sh
+. ${WORKSPACE}/env_vars.sh; export OCS_VERSION=`echo ${UPGRADE_OCS_VERSION} | cut -d "." -f 1-2`; export OCS_CSV_CHANNEL=stable-$OCS_VERSION; yq -y -i ".DEPLOYMENT.ocs_csv_channel |= env.OCS_CSV_CHANNEL" ${WORKSPACE}/ocs-ci-conf.yaml; yq -y -i ".ENV_DATA.ocs_version |= env.OCS_VERSION" ${WORKSPACE}/ocs-ci-conf.yaml ;  sed -i "s|log_dir:.*$|log_dir: ${WORKSPACE}/logs-ocs-ci/"$OCS_VERSION"|g"  ${WORKSPACE}/ocs-ci-conf.yaml
+cat ${WORKSPACE}/ocs-ci-conf.yaml
+. ${WORKSPACE}/env_vars.sh; CURRENT_OCS_VERSION=`echo ${UPGRADE_OCS_VERSION} | cut -d "." -f 1-2`; sed -i "s|export OCS_VERSION=.*$|export OCS_VERSION=${CURRENT_OCS_VERSION}|g" ${WORKSPACE}/nv_vars.sh
+cat ${WORKSPACE}/env_vars.sh
--- scripts/odf/run-odf-upgrade.sh
+++ scripts/odf/run-odf-upgrade.sh
@@ -2,5 +2,5 @@
 ##
 ## This scripts runs ODF upgrade script from the ocs-upi-kvm in the remote bastion node
 ##
-. env_vars.sh; echo "Upgrading ODF to ${UPGRADE_OCS_VERSION}"; cd /root/ocs-upi-kvm/scripts; ./upgrade-ocs-ci.sh > upgrade-ocs-ci.log ;
+. ${WORKSPACE}/env_vars.sh; echo "Upgrading ODF to ${UPGRADE_OCS_VERSION}"; cd ${WORKSPACE}/ocs-upi-kvm/scripts; ./upgrade-ocs-ci.sh > upgrade-ocs-ci.log ;
 odf_csv=`oc get csv -n openshift-storage |grep odf-operator | awk {'print $1'}`; oc get csv $odf_csv -n openshift-storage -o yaml |grep full_version |awk {'print $2'} | tail -n 1 > ${WORKSPACE}/odf-full-build.txt
